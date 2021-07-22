b1e4488287db1d33f179162e2d8dbbc57654e525
Upanayan Baskota <kxahyaar@gmail.com>
2021-07-22 14:37:21 +0545

    minor change

--- src/components/StoryReel.jsx
+++ src/components/StoryReel.jsx
@@ -5,8 +5,8 @@ import Story from './Story.jsx'
 function StoryReel() {
 	return (
 		<div className='storyReel'>
-			<Story image='https://images.unsplash.com/photo-1527333118761-68a5b86be367?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=635&q=80' profileSrc='https://avatars.githubusercontent.com/u/9919?s=460&v=4' title='Upanayan' />
-			<Story image='https://www.enjpg.com/img/2020/retro-29.jpg' profileSrc='https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg' title="Unnayan" />
+			<Story image='https://images.unsplash.com/photo-1527333118761-68a5b86be367?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=635&q=80' profileSrc='https://avatars.githubusercontent.com/u/9919?s=460&v=4' title='B0s0' />
+			<Story image='https://www.enjpg.com/img/2020/retro-29.jpg' profileSrc='https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg' title="not B0s0" />
 			{/* Stories */}
 			{/* Stories */}
 			{/* Stories */}
